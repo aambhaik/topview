6a3873962187aee0f9ecfb4ec359fcde23c151df
Anant Ambhaikar <aambhaik@tibco.com>
2018-06-25 21:38:12 -0700

    change version to 1.0

--- cmd/version.go
+++ cmd/version.go
@@ -15,6 +15,6 @@ var versionCmd = &cobra.Command{
 	Short: "Print the version number of topview CLI",
 	Long:  `All software has versions. This is topview CLI's`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Mashery Topology Viewer v0.1")
+		fmt.Println("Mashery Topology Viewer v1.0")
 	},
 }
